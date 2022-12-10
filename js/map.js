a2cedac26c9bd9778317d5b26c00d0ab9b2f6cfd
ChristinaLast <christina.last@outlook.com>
2022-12-10 18:00:54 -0500

    adding zoom to location and results from incidents

--- js/map.js
+++ js/map.js
@@ -51,16 +51,17 @@ JF.getFormSubmissions("223104390365146", function (response) {
             incidentProps[response[i].answers[answer][lookup]] =
                 response[i].answers[answer].answer;
         });
-        if (incidentProps["Address Map Locator"] == null) {
-            // console.log("hopping out!");
-            continue;
+        if (incidentProps["Location Coordinates"] == null) {
+            response[i].answers[15].answer
         }
+        // incidentProps["Location Coordinates"] = response[i].answers[15].answer
         // convert location coordinates string to float array
-        incidentProps["Address Map Locator"] = incidentProps[
-            "Address Map Locator"
+        incidentProps["Location Coordinates"] = incidentProps[
+            "undefined"
         ]
             .split(/\r?\n/)
             .map((x) => parseFloat(x.replace(/[^\d.-]/g, "")));
+        console.log(incidentProps)
 
         // console.log(submissionProps);
 
@@ -78,7 +79,7 @@ JF.getFormSubmissions("223104390365146", function (response) {
             type: ScatterplotLayer,
             data: incidents,
             getPosition: (d) => {
-                return d["Address Map Locator"];
+                return d["Location Coordinates"];
             },
             // Styles
             radiusUnits: "pixels",
@@ -97,13 +98,168 @@ JF.getFormSubmissions("223104390365146", function (response) {
             onClick: (info) => {
                 //ADD NEW INPUT TO GETIMAGE GALLERY:
                 getImageGallery(info.object.fileUpload, info.object.describeThe);
-                flyToClick(info.object["Address Map Locator"]);
+                flyToClick(info.object["Location Coordinates"]);
             },
         }),
         firstLabelLayerId
-    )
-}
-);
+    );
+    map.addSource("air-data", {
+        type: "geojson",
+        data: "https://opendata.arcgis.com/datasets/1839b35258604422b0b520cbb668df0d_0.geojson",
+    });
+
+    map.addLayer({
+        id: "air",
+        type: "circle",
+        source: "air-data",
+        filled: true,
+        stroke: false,
+        layout: {
+            // Make the layer visible by default.
+            visibility: "visible",
+        },
+    });
+    // Center the map on the coordinates of any clicked circle from the 'circle' layer.
+    map.on("click", "air", (e) => {
+        // console.log("i get here", e.features[0].geometry.coordinates);
+        map.flyTo({
+            center: e.features[0].geometry.coordinates,
+        });
+    });
+    function getImageGallery(images, text) {
+        const imageGallery = document.createElement("div");
+        imageGallery.id = "image-gallery";
+
+        for (var i = 0; i < images.length; i++) {
+            const image = document.createElement("img");
+            image.src = images[i];
+            image.style = "max-width:70%; max-height:70%;";
+            image.className += "img-fluid p-3";
+            imageGallery.appendChild(image);
+        }
+
+        //   add exit button to image gallery
+        const exitButton = document.createElement("button");
+        exitButton.id = "exit-button";
+        exitButton.innerHTML = "X";
+        exitButton.addEventListener("click", () => {
+            document.getElementById("image-gallery").remove();
+        });
+
+        //   stylize the exit button to look good: this can also be a css class
+        exitButton.style.position = "fixed";
+        exitButton.style.top = "15%";
+        exitButton.style.right = "15%";
+        exitButton.style.borderRadius = "0";
+        exitButton.style.padding = "1rem";
+        exitButton.style.fontSize = "2rem";
+        exitButton.style.fontWeight = "bold";
+        exitButton.style.backgroundColor = "white";
+        exitButton.style.border = "none";
+        exitButton.style.cursor = "pointer";
+        exitButton.style.zIndex = "1";
+        imageGallery.appendChild(exitButton);
+
+        //how does each exit button and text correspond to each other?
+        //how do new images get added with each new submission?
+
+        // add text to image gallery
+        const textDiv = document.createElement("div");
+        textDiv.id = "image-gallery-text";
+        textDiv.innerHTML = text;
+
+        // add fixed styling if in modal view
+        textDiv.style.position = "fixed";
+        textDiv.style.top = "15%";
+        textDiv.style.left = "15%";
+        // textDiv.style.right = "0";
+        textDiv.style.borderRadius = "2";
+        textDiv.style.padding = "1rem";
+        textDiv.style.fontSize = "1rem";
+
+        imageGallery.appendChild(textDiv);
+
+        // append the image gallery to the body
+        document.body.appendChild(imageGallery);
+    }
+    function flyToClick(coords) {
+        map.flyTo({
+            center: [coords[0], coords[1]],
+            zoom: 17,
+            essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+        });
+    }
+    // create “current location” function, which doesn’t trigger until called upon.
+    function addUserLocation(latitude, longitude) {
+        return map.addLayer(
+            new MapboxLayer({
+                id: "user-location",
+                type: ScatterplotLayer,
+                data: [{ longitude, latitude }],
+                getPosition: (d) => [d.longitude, d.latitude],
+                getSourceColor: [0, 255, 0],
+                sizeScale: 15,
+                getSize: 10,
+                radiusUnits: "pixels",
+                getRadius: 5,
+                opacity: 0.7,
+                stroked: false,
+                filled: true,
+                radiusScale: 3,
+                getFillColor: [3, 202, 252],
+                parameters: {
+                    depthTest: false,
+                },
+            })
+        );
+    }
+    // get current location
+    const successCallback = (position) => {
+        // add new point layer of current location to deck gl
+        const { latitude, longitude } = position.coords;
+        addUserLocation(latitude, longitude);
+    };
+
+    const errorCallback = (error) => {
+        console.log(error);
+    };
+
+    // create async function to await for current location and then return the promise as lat long coordinates then resolve the promise
+    function getCurrentLocation() {
+        const currentLocation = navigator.geolocation.getCurrentPosition(
+            successCallback,
+            errorCallback
+        );
+        return currentLocation;
+    }
+    if (navigator.geolocation) {
+        getCurrentLocation();
+    }
+
+    const locationButton = document.createElement("div");
+    // create a button that will request the users location
+    locationButton.textContent = "Where am I?";
+    locationButton.id = "location-button";
+    locationButton.addEventListener("click", () => {
+        // when clicked, get the users location
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const { latitude, longitude } = position.coords;
+
+                locationButton.textContent =
+                    "Where am I? " +
+                    position.coords.latitude.toFixed(3) +
+                    ", " +
+                    position.coords.longitude.toFixed(3);
+
+                addUserLocation(latitude, longitude);
+                flyToClick([longitude, latitude]);
+            });
+        }
+    });
+    document.body.appendChild(locationButton);
+});
+
 // Create a function to access the jotform submissions . Format: (formID, callback)
 function getSubmissions() {
     // ENTER YOUR NEW FORM SUBMISSION ID HERE

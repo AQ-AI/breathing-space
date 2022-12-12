df304f0a4d656022f2bf6cd7fb957dc64887d563
Lauren Shrack <lauren@shrack.net>
2022-12-11 22:05:39 -0500

    improved incident report image pop-up

--- js/map.js
+++ js/map.js
@@ -2,7 +2,8 @@ mapboxgl.accessToken = "pk.eyJ1IjoiYWlycG9sbHBoaWxseSIsImEiOiJjbGF3dXRqcTIwamNuM
 const map = new mapboxgl.Map({
     container: "map", // Container ID
     style: "mapbox://styles/airpollphilly/clawv4g4p000014lgrz9qaljy", // Map style to use
-    center: [-75.1652, 39.9526], // center on philadelphia
+    //center: [-75.1652, 39.9526], // center on philadelphia
+    center:[-71.0589, 42.3601],
     zoom: 12, // Starting zoom level
     projection: "globe",
 });
@@ -126,9 +127,11 @@ JF.getFormSubmissions("223104390365146", function (response) {
         });
     });
     function getImageGallery(images, text) {
+      /*
         const imageGallery = document.createElement("div");
         imageGallery.id = "image-gallery";
 
+        
         for (var i = 0; i < images.length; i++) {
             const image = document.createElement("img");
             image.src = images[i];
@@ -136,6 +139,15 @@ JF.getFormSubmissions("223104390365146", function (response) {
             image.className += "img-fluid p-3";
             imageGallery.appendChild(image);
         }
+
+        const image_div = document.createElement("div")
+        image_div.className = "image-gallery-image-div"
+        const image = document.createElement("img")
+        image.src = images[0]
+        image.style = "max-width:30%;"
+        image_div.appendChild(image)
+
+        imageGallery.appendChild(image_div)
 
         //   add exit button to image gallery
         const exitButton = document.createElement("button");
@@ -164,19 +176,46 @@ JF.getFormSubmissions("223104390365146", function (response) {
 
         // add text to image gallery
         const textDiv = document.createElement("div");
+        textDiv.className = "image-gallery-text";
         textDiv.id = "image-gallery-text";
         textDiv.innerHTML = text;
 
         // add fixed styling if in modal view
-        textDiv.style.position = "fixed";
-        textDiv.style.top = "15%";
-        textDiv.style.left = "15%";
+        //textDiv.style.position = "fixed";
+        //textDiv.style.top = "15%";
+        //textDiv.style.left = "15%";
         // textDiv.style.right = "0";
         textDiv.style.borderRadius = "2";
         textDiv.style.padding = "1rem";
         textDiv.style.fontSize = "1rem";
 
         imageGallery.appendChild(textDiv);
+
+        */
+
+        const imageGallery = document.createElement("div")
+        imageGallery.className = "image-gallery"
+        imageGallery.id = "image-gallery"
+
+        const image = document.createElement("img")
+        image.src = images[0]
+        image.className = "image-gallery-image"
+        imageGallery.appendChild(image)
+
+        const exitButton = document.createElement("button");
+        exitButton.className = "image-gallery-button";
+        exitButton.id = "image-gallery-button";
+        exitButton.innerHTML = "X";
+        exitButton.addEventListener("click", () => {
+            document.getElementById("image-gallery").remove();
+        });
+        imageGallery.appendChild(exitButton)
+
+        const textDiv = document.createElement("div");
+        textDiv.className = "image-gallery-text";
+        textDiv.id = "image-gallery-text";
+        textDiv.innerHTML = "<i>\"" + text + "\"</i>";
+        imageGallery.appendChild(textDiv)
 
         // append the image gallery to the body
         document.body.appendChild(imageGallery);
